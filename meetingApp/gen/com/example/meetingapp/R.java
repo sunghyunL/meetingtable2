c4940dd8c8eead2de5f0f3018d2a6b8d448cbde3
sunghyun <lshkill7@naver.com>
2013-08-24 12:27:11 +0900

    성현수정1

--- meetingApp/gen/com/example/meetingapp/R.java
+++ meetingApp/gen/com/example/meetingapp/R.java
@@ -82,6 +82,7 @@ public final class R {
         public static final int gallery_main=0x7f030007;
         public static final int griditemcopy=0x7f030008;
         public static final int mainmenu=0x7f030009;
+        public static final int scan_main=0x7f03000a;
     }
     public static final class menu {
         public static final int main=0x7f090000;
